7d37db658a7ae70f85d7b65f7bfdb1ca92ab1660
Sanjida Aktar <mst.sanjida.aktar.cnj.bd@gmail.com>
2025-11-22 23:06:09 +0700

    Add files via upload

---
+++ C/1769_Ho Ho Ho.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++)
+    {
+        printf("Ho");
+        if(i==n-1)
+            printf("!\n");
+        else
+            printf(" ");
+    }
+    return 0;
+}
